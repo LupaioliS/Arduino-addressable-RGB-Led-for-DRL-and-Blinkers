37427a896fa39678ca18249617d6c5b55f103b45
LupaioliS <33904836+LupaioliS@users.noreply.github.com>
2023-02-22 19:57:34 +0100

    Removed commented lines
    Not useful anymore

--- main/main.ino
+++ main/main.ino
@@ -10,45 +10,5 @@ void setup() {
 } 
 
 void loop() {
-  /*for (int i = 0; i <= 143; i++) {
-	leds[i] = CRGB ( 0, 0, 0);
-  }
-  FastLED.show();
-  delay(100);
-  for (int i = 20; i >= 0; i--) {
-	leds[i] = CRGB (255, 35, 0);
-	FastLED.show();
-	delay(30);
-  }
-  delay(300);
 
-  for (int i = 0; i <= 143; i++) {
-	leds[i] = CRGB ( 0, 0, 0);
-  }
-  FastLED.show();
-  delay(100);
-  for (int i = 20; i >= 0; i--) {
-	leds[i] = CRGB (255, 35, 0);
-	FastLED.show();
-	delay(30);
-  }
-  delay(300);
-
-  for (int i = 0; i <= 143; i++) {
-	leds[i] = CRGB ( 0, 0, 0);
-  }
-  FastLED.show();
-  delay(100);
-  for (int i = 20; i >= 0; i--) {
-	leds[i] = CRGB (255, 35, 0);
-	FastLED.show();
-	delay(30);
-  }
-  delay(300);
-
-  for (int i = 0; i <= 20; i++) {
-	leds[i] = CRGB ( 255, 255, 255);
-  }
-  FastLED.show();
-  delay(3000);*/
 }
