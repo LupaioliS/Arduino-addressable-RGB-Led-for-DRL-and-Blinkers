7cd7674638b5ccc812044886a0866e4b79e87d7f
LupaioliS <33904836+LupaioliS@users.noreply.github.com>
2023-02-24 00:59:15 +0100

    Added animation stuff

--- main/DRLBlinker.h
+++ main/DRLBlinker.h
@@ -7,13 +7,14 @@
 	//#define NUM_ARGS_(_1, _2, _3, _4, _5, _6, _7, _8, _9, _10, _11, _12, _13, _14, _15, _16, _17, _18, _19, _20, _21, _22, _23, _24, _25, _26, _27, _28, _29, _30, _31, _32, _33, _34, _35, _36, _37, _38, _39, _40, _41, _42, _43, _44, _45, _46, _47, _48, _49, _50, N, ...) N
 	//#define COUNT_ARGS(...) NUM_ARGS_(__VA_ARGS__, 50, 49, 48, 47, 46, 45, 44, 43, 42, 41, 40, 39, 38, 37, 36, 35, 34, 33, 32, 31, 30, 29, 28, 27, 26, 25, 24, 23, 22, 21, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1)
 
-	#define RESET {1000, CRGB(0, 0, 0)}
-	#define PIXEL(n, r, g, b) {n, CRGB(r, g, b)}
+	#define ALL_RESET {1000, CHSV(0, 0, 0)}
+	#define ALL_FADEOUT {1001, CHSV(0, 0, 0)}
+	#define PIXEL(n, r, g, b) {n, CHSV(r, g, b)}
 	#define FRAME(N, PX...) {N, new Pixel[N] { PX }}
 	
 	struct Pixel {
 		uint16_t led_index;
-		CRGB color;
+		CHSV color;
 	};
 
 	struct Frame {
@@ -25,31 +26,35 @@
 
 	template <uint8_t DATA_PIN, uint16_t NUM_LEDS> class DRLBlinker {
 		private:
-			CRGB DRLColor;
+			CHSV DRLColor;
 			CRGB _leds[NUM_LEDS];
 
 			uint16_t animation_speed = 500;
-			bool animation_active = false;
 			uint16_t current_frame = 0;
 			uint16_t max_frames;
 			Frame* current_animation;
+			int current_animation_id;
+			volatile unsigned long last_frame_time;
+			void (*onAnimationEnd)(int);
 
 		public:
 			const char* DRLName;
-		
+			bool animation_active = false;
 
-			DRLBlinker(const char* name, CRGB color) : DRLName(name) {
+			DRLBlinker(const char* name, CHSV color) : DRLName(name) {
 				setDRLColor(color);
 			}	
 
-			void initialize() {
+			void initialize(int (*callback)(int)) {
 				FastLED.addLeds<WS2812, DATA_PIN, GRB>(_leds, NUM_LEDS);
 
+				onAnimationEnd = callback;
+
 				reset();
-				showDRL();
+				//showDRL();
 			}
 
-			void setDRLColor(CRGB color) {
+			void setDRLColor(CHSV color) {
 				DRLColor = color;
 			}
 
@@ -59,8 +64,12 @@
 			}
 
 			void showDRL() {
-				fill_solid(_leds, NUM_LEDS, DRLColor);
-				FastLED.show();
+				Serial.println("showDRL");
+				for(int i = 0; i < DRLColor.value; i++) {
+					if(animation_active == true) break;
+					fill_solid(_leds, NUM_LEDS, CHSV(DRLColor.hue, DRLColor.saturation, i));
+					FastLED.show();
+				}
 			}
 
 			uint16_t nextFrameIndex() {
@@ -71,20 +80,27 @@
 				}
 			}
 
-			void setupAnimation(Frame* animation, uint16_t animation_size, uint16_t speed) {
+			void setupAnimation(int animationID, Frame* animation, uint16_t animation_size, uint16_t speed) {
+				Serial.println("setupAnimation");
+				deleteAnimationMemory();
 				animation_speed = speed;
 				current_animation = animation;
 				max_frames = animation_size;
+				current_animation_id = animationID;
 			}
 
-			void startAnimation() {
-				current_frame = 0;
-				animation_active = true;
+			void startAnimation(int animationID) {
+				if(animationID == current_animation_id) {
+					current_frame = 0;
+					animation_active = true;
+				}
 			}
 
 			void stopAnimation() {
 				current_frame = 0;
 				animation_active = false;
+
+				onAnimationEnd(current_animation_id);
 			}
 
 			void deleteAnimationMemory() {
@@ -95,8 +111,41 @@
 			}
 
 			void render() {
+				volatile unsigned long currentTime = millis();
+
 				if(animation_active) {
-					EVERY_N_MILLISECONDS(animation_speed) {
+					if(currentTime - last_frame_time > animation_speed) {
+						last_frame_time = currentTime;
+						for(int i = 0; i < current_animation[current_frame].frame_size; i++) {
+							switch(current_animation[current_frame].pixels[i].led_index) {
+								case 1000: { //ALL_RESET
+									reset();
+									break;
+								}
+								case 1001: { //ALL_FADEOUT
+									reset();
+									break;
+								}
+								default: {
+									_leds[current_animation[current_frame].pixels[i].led_index] = current_animation[current_frame].pixels[i].color;
+									break;
+								}
+							}
+						}
+
+						FastLED.show();
+
+						const uint16_t next_frame = nextFrameIndex();
+						if(next_frame == -1) {
+							stopAnimation();
+						} else {
+							current_frame = next_frame;
+						}
+					}
+
+					/*EVERY_N_MILLISECONDS(animation_speed) {
+						Serial.print("animation speed: ");
+						Serial.println(animation_speed);
 						for(int i = 0; i < current_animation[current_frame].frame_size; i++) {
 							switch(current_animation[current_frame].pixels[i].led_index) {
 								case 1000: {
@@ -118,11 +167,6 @@
 						} else {
 							current_frame = next_frame;
 						}
-					}
-				} else {
-					/*if(sizeof(current_animation) == 0) {
-						_leds[0] = CRGB(0, 255, 0);
-						FastLED.show();
 					}*/
 				}
 			}
--- main/main.ino
+++ main/main.ino
@@ -3,144 +3,142 @@
 #define LEDS_PER_STRIP 31
 
 
-DRLBlinker<7, LEDS_PER_STRIP> lxDRL("lxDRL", CRGB(0, 0, 0));
+DRLBlinker<7, LEDS_PER_STRIP> lxDRL("lxDRL", CHSV(0, 0, 255));
 //DRLBlinker<8, LEDS_PER_STRIP> rxDRL("rxDRL", CRGB(255,255,255));
 
 
 
-/*Frame singleFrame[] = {
-	RESET,
-	{0, 0, 	CRGB(255, 35, 0)}, 
-	{1, 0, 	CRGB(255, 35, 0)}, 
-	{2, 0, 	CRGB(255, 35, 0)}, 
-	{3, 0, 	CRGB(255, 35, 0)},
-	{4, 0, 	CRGB(255, 35, 0)},
-	{5, 0, 	CRGB(255, 35, 0)},
-	{6, 0, 	CRGB(255, 35, 0)},
-	{7, 0, 	CRGB(255, 35, 0)},
-	{8, 0, 	CRGB(255, 35, 0)},
-	{9, 0, 	CRGB(255, 35, 0)},
-	{10, 0, CRGB(255, 35, 0)},
-	{11, 0, CRGB(255, 35, 0)},
-	{12, 0, CRGB(255, 35, 0)},
-	{13, 0, CRGB(255, 35, 0)},
-	{14, 0, CRGB(255, 35, 0)},
-	{15, 0, CRGB(255, 35, 0)},
-	{16, 0, CRGB(255, 35, 0)},
-	{17, 0, CRGB(255, 35, 0)},
-	{18, 0, CRGB(255, 35, 0)},
-	{19, 0, CRGB(255, 35, 0)},
-	{20, 0, CRGB(255, 35, 0)},
-	{21, 0, CRGB(255, 35, 0)},
-	{22, 0, CRGB(255, 35, 0)},
-	{23, 0, CRGB(255, 35, 0)},
-	{24, 0, CRGB(255, 35, 0)},
-	{25, 0, CRGB(255, 35, 0)},
-	{26, 0, CRGB(255, 35, 0)},
-	{27, 0, CRGB(255, 35, 0)},
-	{28, 0, CRGB(255, 35, 0)},
-	{29, 0, CRGB(255, 35, 0)}
-};
-*/
-
 #define BLINK_FRAMES 32
 Frame blink[BLINK_FRAMES] = {
-	{2, new Pixel[2] { 
-		RESET, {0, CRGB(255, 35, 0)}
-	}},
-	{1, new Pixel[1] { 
-		{1, CRGB(255, 35, 0)}
-	}},
-	{1, new Pixel[1] { 
-		{2, CRGB(255, 35, 0)}
-	}},
-	{1, new Pixel[1] { 
-		{3, CRGB(255, 35, 0)}
-	}},
-	{1, new Pixel[1] { 
-		{4, CRGB(255, 35, 0)}
-	}},
-	{1, new Pixel[1] { 
-		{5, CRGB(255, 35, 0)}
-	}},
-	{1, new Pixel[1] { 
-		{6, CRGB(255, 35, 0)}
-	}},
-	{1, new Pixel[1] { 
-		{7, CRGB(255, 35, 0)}
-	}},
-	{1, new Pixel[1] { 
-		{8, CRGB(255, 35, 0)}
-	}},
-	{1, new Pixel[1] { 
-		{9, CRGB(255, 35, 0)}
-	}},
-	{1, new Pixel[1] { 
-		{10, CRGB(255, 35, 0)}
-	}},
-	{1, new Pixel[1] { 
-		{11, CRGB(255, 35, 0)}
-	}},
-	{1, new Pixel[1] { 
-		{12, CRGB(255, 35, 0)}
-	}},
-	{1, new Pixel[1] { 
-		{13, CRGB(255, 35, 0)}
-	}},
-	{1, new Pixel[1] { 
-		{14, CRGB(255, 35, 0)}
-	}},
-	{1, new Pixel[1] { 
-		{15, CRGB(255, 35, 0)}
-	}},
-	{1, new Pixel[1] { 
-		{16, CRGB(255, 35, 0)}
-	}},
-	{1, new Pixel[1] { 
-		{17, CRGB(255, 35, 0)}
-	}},
-	{1, new Pixel[1] { 
-		{18, CRGB(255, 35, 0)}
-	}},
-	{1, new Pixel[1] { 
-		{19, CRGB(255, 35, 0)}
-	}},
-	{1, new Pixel[1] { 
-		{20, CRGB(255, 35, 0)}
-	}},
-	{1, new Pixel[1] { 
-		{21, CRGB(255, 35, 0)}
-	}},
-	{1, new Pixel[1] { 
-		{22, CRGB(255, 35, 0)}
-	}},
-	{1, new Pixel[1] { 
-		{23, CRGB(255, 35, 0)}
-	}},
-	{1, new Pixel[1] { 
-		{24, CRGB(255, 35, 0)}
-	}},
-	{1, new Pixel[1] { 
-		{25, CRGB(255, 35, 0)}
-	}},
-	{1, new Pixel[1] { 
-		{26, CRGB(255, 35, 0)}
-	}},
-	{1, new Pixel[1] { 
-		{27, CRGB(255, 35, 0)}
-	}},
-	{1, new Pixel[1] { 
-		{28, CRGB(255, 35, 0)}
-	}},
-	{1, new Pixel[1] { 
-		{29, CRGB(255, 35, 0)}
-	}},
-	{1, new Pixel[1] { 
-		{30, CRGB(255, 35, 0)}
-	}},
-	{1, new Pixel[1] { 
-		RESET
-	}}
+	FRAME(
+		2, 
+		ALL_RESET, 
+		PIXEL(0, 8, 255, 255)
+	),
+	FRAME(
+		1,
+		PIXEL(1, 8, 255, 255)
+	),
+	FRAME(
+		1, 
+		PIXEL(2, 8, 255, 255)
+	),
+	FRAME(
+		1, 
+		PIXEL(3, 8, 255, 255)
+	),
+	FRAME(
+		1, 
+		PIXEL(4, 8, 255, 255)
+	),
+	FRAME(
+		1, 
+		PIXEL(5, 8, 255, 255)
+	),
+	FRAME(
+		1, 
+		PIXEL(6, 8, 255, 255)
+	),
+	FRAME(
+		1, 
+		PIXEL(7, 8, 255, 255)
+	),
+	FRAME(
+		1, 
+		PIXEL(8, 8, 255, 255)
+	),
+	FRAME(
+		1, 
+		PIXEL(9, 8, 255, 255)
+	),
+	FRAME(
+		1, 
+		PIXEL(10, 8, 255, 255)
+	),
+	FRAME(
+		1, 
+		PIXEL(11, 8, 255, 255)
+	),
+	FRAME(
+		1, 
+		PIXEL(12, 8, 255, 255)
+	),
+	FRAME(
+		1, 
+		PIXEL(13, 8, 255, 255)
+	),
+	FRAME(
+		1, 
+		PIXEL(14, 8, 255, 255)
+	),
+	FRAME(
+		1, 
+		PIXEL(15, 8, 255, 255)
+	),
+	FRAME(
+		1, 
+		PIXEL(16, 8, 255, 255)
+	),
+	FRAME(
+		1, 
+		PIXEL(17, 8, 255, 255)
+	),
+	FRAME(
+		1, 
+		PIXEL(18, 8, 255, 255)
+	),
+	FRAME(
+		1, 
+		PIXEL(19, 8, 255, 255)
+	),
+	FRAME(
+		1, 
+		PIXEL(20, 8, 255, 255)
+	),
+	FRAME(
+		1, 
+		PIXEL(21, 8, 255, 255)
+	),
+	FRAME(
+		1, 
+		PIXEL(22, 8, 255, 255)
+	),
+	FRAME(
+		1, 
+		PIXEL(23, 8, 255, 255)
+	),
+	FRAME(
+		1, 
+		PIXEL(24, 8, 255, 255)
+	),
+	FRAME(
+		1, 
+		PIXEL(25, 8, 255, 255)
+	),
+	FRAME(
+		1, 
+		PIXEL(26, 8, 255, 255)
+	),
+	FRAME(
+		1, 
+		PIXEL(27, 8, 255, 255)
+	),
+	FRAME(
+		1, 
+		PIXEL(28, 8, 255, 255)
+	),
+	FRAME(
+		1, 
+		PIXEL(29, 8, 255, 255)
+	),
+	FRAME(
+		1, 
+		PIXEL(30, 8, 255, 255)
+	),
+	FRAME(
+		1,
+		ALL_RESET
+	)
 };
 
 
@@ -164,246 +162,246 @@ Frame blink[BLINK_FRAMES] = {
   REP##ONES(X)*/
 
 
-#define STARTUP_FRAMES 16
+#define STARTUP_FRAMES 14
 Frame startup[STARTUP_FRAMES] = {
 	FRAME(
 		2,
-		RESET,
-		PIXEL(15, 20, 20, 20)
+		ALL_RESET,
+		PIXEL(15, 213, 255, 255)
 	),
 	FRAME(
 		3,
-		PIXEL(14, 20, 20, 20),
-		PIXEL(15, 20, 20, 20),
-		PIXEL(16, 20, 20, 20)
+		PIXEL(14, 213, 255, 255),
+		PIXEL(15, 0, 0, 50),
+		PIXEL(16, 213, 255, 255)
 	),
 	FRAME(
 		5,
-		PIXEL(13, 20, 20, 20),
-		PIXEL(14, 20, 20, 20),
-		PIXEL(15, 20, 20, 20),
-		PIXEL(16, 20, 20, 20),
-		PIXEL(17, 20, 20, 20)
+		PIXEL(13, 213, 255, 255),
+		PIXEL(14, 0, 0, 50),
+		PIXEL(15, 0, 0, 50),
+		PIXEL(16, 0, 0, 50),
+		PIXEL(17, 213, 255, 255)
 	),
 	FRAME(
 		7,
-		PIXEL(12, 20, 20, 20),
-		PIXEL(13, 20, 20, 20),
-		PIXEL(14, 20, 20, 20),
-		PIXEL(15, 20, 20, 20),
-		PIXEL(16, 20, 20, 20),
-		PIXEL(17, 20, 20, 20),
-		PIXEL(18, 20, 20, 20)
+		PIXEL(12, 213, 255, 255),
+		PIXEL(13, 0, 0, 50),
+		PIXEL(14, 0, 0, 50),
+		PIXEL(15, 0, 0, 50),
+		PIXEL(16, 0, 0, 50),
+		PIXEL(17, 0, 0, 50),
+		PIXEL(18, 213, 255, 255)
 	),
 	FRAME(
 		9,
-		PIXEL(11, 20, 20, 20),
-		PIXEL(12, 20, 20, 20),
-		PIXEL(13, 20, 20, 20),
-		PIXEL(14, 20, 20, 20),
-		PIXEL(15, 20, 20, 20),
-		PIXEL(16, 20, 20, 20),
-		PIXEL(17, 20, 20, 20),
-		PIXEL(18, 20, 20, 20),
-		PIXEL(19, 20, 20, 20)
+		PIXEL(11, 213, 255, 255),
+		PIXEL(12, 0, 0, 50),
+		PIXEL(13, 0, 0, 50),
+		PIXEL(14, 0, 0, 50),
+		PIXEL(15, 0, 0, 50),
+		PIXEL(16, 0, 0, 50),
+		PIXEL(17, 0, 0, 50),
+		PIXEL(18, 0, 0, 50),
+		PIXEL(19, 213, 255, 255)
 	),
 	FRAME(
 		11,
-		PIXEL(10, 20, 20, 20),
-		PIXEL(11, 20, 20, 20),
-		PIXEL(12, 20, 20, 20),
-		PIXEL(13, 20, 20, 20),
-		PIXEL(14, 20, 20, 20),
-		PIXEL(15, 20, 20, 20),
-		PIXEL(16, 20, 20, 20),
-		PIXEL(17, 20, 20, 20),
-		PIXEL(18, 20, 20, 20),
-		PIXEL(19, 20, 20, 20),
-		PIXEL(20, 20, 20, 20)
+		PIXEL(10, 213, 255, 255),
+		PIXEL(11, 0, 0, 50),
+		PIXEL(12, 0, 0, 50),
+		PIXEL(13, 0, 0, 50),
+		PIXEL(14, 0, 0, 50),
+		PIXEL(15, 0, 0, 50),
+		PIXEL(16, 0, 0, 50),
+		PIXEL(17, 0, 0, 50),
+		PIXEL(18, 0, 0, 50),
+		PIXEL(19, 0, 0, 50),
+		PIXEL(20, 213, 255, 255)
 	),
 	FRAME(
 		13,
-		PIXEL(9, 20, 20, 20),
-		PIXEL(10, 20, 20, 20),
-		PIXEL(11, 20, 20, 20),
-		PIXEL(12, 20, 20, 20),
-		PIXEL(13, 20, 20, 20),
-		PIXEL(14, 20, 20, 20),
-		PIXEL(15, 20, 20, 20),
-		PIXEL(16, 20, 20, 20),
-		PIXEL(17, 20, 20, 20),
-		PIXEL(18, 20, 20, 20),
-		PIXEL(19, 20, 20, 20),
-		PIXEL(20, 20, 20, 20),
-		PIXEL(21, 20, 20, 20)
+		PIXEL(9, 213, 255, 255),
+		PIXEL(10, 0, 0, 50),
+		PIXEL(11, 0, 0, 50),
+		PIXEL(12, 0, 0, 50),
+		PIXEL(13, 0, 0, 50),
+		PIXEL(14, 0, 0, 50),
+		PIXEL(15, 0, 0, 50),
+		PIXEL(16, 0, 0, 50),
+		PIXEL(17, 0, 0, 50),
+		PIXEL(18, 0, 0, 50),
+		PIXEL(19, 0, 0, 50),
+		PIXEL(20, 0, 0, 50),
+		PIXEL(21, 213, 255, 255)
 	),
 	FRAME(
 		15,
-		PIXEL(8, 20, 20, 20),
-		PIXEL(9, 20, 20, 20),
-		PIXEL(10, 20, 20, 20),
-		PIXEL(11, 20, 20, 20),
-		PIXEL(12, 20, 20, 20),
-		PIXEL(13, 20, 20, 20),
-		PIXEL(14, 20, 20, 20),
-		PIXEL(15, 20, 20, 20),
-		PIXEL(16, 20, 20, 20),
-		PIXEL(17, 20, 20, 20),
-		PIXEL(18, 20, 20, 20),
-		PIXEL(19, 20, 20, 20),
-		PIXEL(20, 20, 20, 20),
-		PIXEL(21, 20, 20, 20),
-		PIXEL(22, 20, 20, 20)
+		PIXEL(8, 213, 255, 255),
+		PIXEL(9, 0, 0, 50),
+		PIXEL(10, 0, 0, 50),
+		PIXEL(11, 0, 0, 50),
+		PIXEL(12, 0, 0, 50),
+		PIXEL(13, 0, 0, 50),
+		PIXEL(14, 0, 0, 50),
+		PIXEL(15, 0, 0, 50),
+		PIXEL(16, 0, 0, 50),
+		PIXEL(17, 0, 0, 50),
+		PIXEL(18, 0, 0, 50),
+		PIXEL(19, 0, 0, 50),
+		PIXEL(20, 0, 0, 50),
+		PIXEL(21, 0, 0, 50),
+		PIXEL(22, 213, 255, 255)
 	),
 	FRAME(
 		17,
-		PIXEL(7, 20, 20, 20),
-		PIXEL(8, 20, 20, 20),
-		PIXEL(9, 20, 20, 20),
-		PIXEL(10, 20, 20, 20),
-		PIXEL(11, 20, 20, 20),
-		PIXEL(12, 20, 20, 20),
-		PIXEL(13, 20, 20, 20),
-		PIXEL(14, 20, 20, 20),
-		PIXEL(15, 20, 20, 20),
-		PIXEL(16, 20, 20, 20),
-		PIXEL(17, 20, 20, 20),
-		PIXEL(18, 20, 20, 20),
-		PIXEL(19, 20, 20, 20),
-		PIXEL(20, 20, 20, 20),
-		PIXEL(21, 20, 20, 20),
-		PIXEL(22, 20, 20, 20),
-		PIXEL(23, 20, 20, 20)
+		PIXEL(7, 213, 255, 255),
+		PIXEL(8, 0, 0, 50),
+		PIXEL(9, 0, 0, 50),
+		PIXEL(10, 0, 0, 50),
+		PIXEL(11, 0, 0, 50),
+		PIXEL(12, 0, 0, 50),
+		PIXEL(13, 0, 0, 50),
+		PIXEL(14, 0, 0, 50),
+		PIXEL(15, 0, 0, 50),
+		PIXEL(16, 0, 0, 50),
+		PIXEL(17, 0, 0, 50),
+		PIXEL(18, 0, 0, 50),
+		PIXEL(19, 0, 0, 50),
+		PIXEL(20, 0, 0, 50),
+		PIXEL(21, 0, 0, 50),
+		PIXEL(22, 0, 0, 50),
+		PIXEL(23, 213, 255, 255)
 	),
 	FRAME(
 		19,
-		PIXEL(6, 20, 20, 20),
-		PIXEL(7, 20, 20, 20),
-		PIXEL(8, 20, 20, 20),
-		PIXEL(9, 20, 20, 20),
-		PIXEL(10, 20, 20, 20),
-		PIXEL(11, 20, 20, 20),
-		PIXEL(12, 20, 20, 20),
-		PIXEL(13, 20, 20, 20),
-		PIXEL(14, 20, 20, 20),
-		PIXEL(15, 20, 20, 20),
-		PIXEL(16, 20, 20, 20),
-		PIXEL(17, 20, 20, 20),
-		PIXEL(18, 20, 20, 20),
-		PIXEL(19, 20, 20, 20),
-		PIXEL(20, 20, 20, 20),
-		PIXEL(21, 20, 20, 20),
-		PIXEL(22, 20, 20, 20),
-		PIXEL(23, 20, 20, 20),
-		PIXEL(24, 20, 20, 20)
+		PIXEL(6, 213, 255, 255),
+		PIXEL(7, 0, 0, 50),
+		PIXEL(8, 0, 0, 50),
+		PIXEL(9, 0, 0, 50),
+		PIXEL(10, 0, 0, 50),
+		PIXEL(11, 0, 0, 50),
+		PIXEL(12, 0, 0, 50),
+		PIXEL(13, 0, 0, 50),
+		PIXEL(14, 0, 0, 50),
+		PIXEL(15, 0, 0, 50),
+		PIXEL(16, 0, 0, 50),
+		PIXEL(17, 0, 0, 50),
+		PIXEL(18, 0, 0, 50),
+		PIXEL(19, 0, 0, 50),
+		PIXEL(20, 0, 0, 50),
+		PIXEL(21, 0, 0, 50),
+		PIXEL(22, 0, 0, 50),
+		PIXEL(23, 0, 0, 50),
+		PIXEL(24, 213, 255, 255)
 	),
 	FRAME(
 		21,
-		PIXEL(5, 20, 20, 20),
-		PIXEL(6, 20, 20, 20),
-		PIXEL(7, 20, 20, 20),
-		PIXEL(8, 20, 20, 20),
-		PIXEL(9, 20, 20, 20),
-		PIXEL(10, 20, 20, 20),
-		PIXEL(11, 20, 20, 20),
-		PIXEL(12, 20, 20, 20),
-		PIXEL(13, 20, 20, 20),
-		PIXEL(14, 20, 20, 20),
-		PIXEL(15, 20, 20, 20),
-		PIXEL(16, 20, 20, 20),
-		PIXEL(17, 20, 20, 20),
-		PIXEL(18, 20, 20, 20),
-		PIXEL(19, 20, 20, 20),
-		PIXEL(20, 20, 20, 20),
-		PIXEL(21, 20, 20, 20),
-		PIXEL(22, 20, 20, 20),
-		PIXEL(23, 20, 20, 20),
-		PIXEL(24, 20, 20, 20),
-		PIXEL(25, 20, 20, 20)
+		PIXEL(5, 213, 255, 255),
+		PIXEL(6, 0, 0, 50),
+		PIXEL(7, 0, 0, 50),
+		PIXEL(8, 0, 0, 50),
+		PIXEL(9, 0, 0, 50),
+		PIXEL(10, 0, 0, 50),
+		PIXEL(11, 0, 0, 50),
+		PIXEL(12, 0, 0, 50),
+		PIXEL(13, 0, 0, 50),
+		PIXEL(14, 0, 0, 50),
+		PIXEL(15, 0, 0, 50),
+		PIXEL(16, 0, 0, 50),
+		PIXEL(17, 0, 0, 50),
+		PIXEL(18, 0, 0, 50),
+		PIXEL(19, 0, 0, 50),
+		PIXEL(20, 0, 0, 50),
+		PIXEL(21, 0, 0, 50),
+		PIXEL(22, 0, 0, 50),
+		PIXEL(23, 0, 0, 50),
+		PIXEL(24, 0, 0, 50),
+		PIXEL(25, 213, 255, 255)
 	),
 	FRAME(
 		23,
-		PIXEL(4, 20, 20, 20),
-		PIXEL(5, 20, 20, 20),
-		PIXEL(6, 20, 20, 20),
-		PIXEL(7, 20, 20, 20),
-		PIXEL(8, 20, 20, 20),
-		PIXEL(9, 20, 20, 20),
-		PIXEL(10, 20, 20, 20),
-		PIXEL(11, 20, 20, 20),
-		PIXEL(12, 20, 20, 20),
-		PIXEL(13, 20, 20, 20),
-		PIXEL(14, 20, 20, 20),
-		PIXEL(15, 20, 20, 20),
-		PIXEL(16, 20, 20, 20),
-		PIXEL(17, 20, 20, 20),
-		PIXEL(18, 20, 20, 20),
-		PIXEL(19, 20, 20, 20),
-		PIXEL(20, 20, 20, 20),
-		PIXEL(21, 20, 20, 20),
-		PIXEL(22, 20, 20, 20),
-		PIXEL(23, 20, 20, 20),
-		PIXEL(24, 20, 20, 20),
-		PIXEL(25, 20, 20, 20),
-		PIXEL(26, 20, 20, 20)
+		PIXEL(4, 213, 255, 255),
+		PIXEL(5, 0, 0, 50),
+		PIXEL(6, 0, 0, 50),
+		PIXEL(7, 0, 0, 50),
+		PIXEL(8, 0, 0, 50),
+		PIXEL(9, 0, 0, 50),
+		PIXEL(10, 0, 0, 50),
+		PIXEL(11, 0, 0, 50),
+		PIXEL(12, 0, 0, 50),
+		PIXEL(13, 0, 0, 50),
+		PIXEL(14, 0, 0, 50),
+		PIXEL(15, 0, 0, 50),
+		PIXEL(16, 0, 0, 50),
+		PIXEL(17, 0, 0, 50),
+		PIXEL(18, 0, 0, 50),
+		PIXEL(19, 0, 0, 50),
+		PIXEL(20, 0, 0, 50),
+		PIXEL(21, 0, 0, 50),
+		PIXEL(22, 0, 0, 50),
+		PIXEL(23, 0, 0, 50),
+		PIXEL(24, 0, 0, 50),
+		PIXEL(25, 0, 0, 50),
+		PIXEL(26, 213, 255, 255)
 	),
 	FRAME(
 		25,
-		PIXEL(3, 20, 20, 20),
-		PIXEL(4, 20, 20, 20),
-		PIXEL(5, 20, 20, 20),
-		PIXEL(6, 20, 20, 20),
-		PIXEL(7, 20, 20, 20),
-		PIXEL(8, 20, 20, 20),
-		PIXEL(9, 20, 20, 20),
-		PIXEL(10, 20, 20, 20),
-		PIXEL(11, 20, 20, 20),
-		PIXEL(12, 20, 20, 20),
-		PIXEL(13, 20, 20, 20),
-		PIXEL(14, 20, 20, 20),
-		PIXEL(15, 20, 20, 20),
-		PIXEL(16, 20, 20, 20),
-		PIXEL(17, 20, 20, 20),
-		PIXEL(18, 20, 20, 20),
-		PIXEL(19, 20, 20, 20),
-		PIXEL(20, 20, 20, 20),
-		PIXEL(21, 20, 20, 20),
-		PIXEL(22, 20, 20, 20),
-		PIXEL(23, 20, 20, 20),
-		PIXEL(24, 20, 20, 20),
-		PIXEL(25, 20, 20, 20),
-		PIXEL(26, 20, 20, 20),
-		PIXEL(27, 20, 20, 20)
+		PIXEL(3, 213, 255, 255),
+		PIXEL(4, 0, 0, 50),
+		PIXEL(5, 0, 0, 50),
+		PIXEL(6, 0, 0, 50),
+		PIXEL(7, 0, 0, 50),
+		PIXEL(8, 0, 0, 50),
+		PIXEL(9, 0, 0, 50),
+		PIXEL(10, 0, 0, 50),
+		PIXEL(11, 0, 0, 50),
+		PIXEL(12, 0, 0, 50),
+		PIXEL(13, 0, 0, 50),
+		PIXEL(14, 0, 0, 50),
+		PIXEL(15, 0, 0, 50),
+		PIXEL(16, 0, 0, 50),
+		PIXEL(17, 0, 0, 50),
+		PIXEL(18, 0, 0, 50),
+		PIXEL(19, 0, 0, 50),
+		PIXEL(20, 0, 0, 50),
+		PIXEL(21, 0, 0, 50),
+		PIXEL(22, 0, 0, 50),
+		PIXEL(23, 0, 0, 50),
+		PIXEL(24, 0, 0, 50),
+		PIXEL(25, 0, 0, 50),
+		PIXEL(26, 0, 0, 50),
+		PIXEL(27, 213, 255, 255)
 	),
 	FRAME(
 		27,
-		PIXEL(2, 20, 20, 20),
-		PIXEL(3, 20, 20, 20),
-		PIXEL(4, 20, 20, 20),
-		PIXEL(5, 20, 20, 20),
-		PIXEL(6, 20, 20, 20),
-		PIXEL(7, 20, 20, 20),
-		PIXEL(8, 20, 20, 20),
-		PIXEL(9, 20, 20, 20),
-		PIXEL(10, 20, 20, 20),
-		PIXEL(11, 20, 20, 20),
-		PIXEL(12, 20, 20, 20),
-		PIXEL(13, 20, 20, 20),
-		PIXEL(14, 20, 20, 20),
-		PIXEL(15, 20, 20, 20),
-		PIXEL(16, 20, 20, 20),
-		PIXEL(17, 20, 20, 20),
-		PIXEL(18, 20, 20, 20),
-		PIXEL(19, 20, 20, 20),
-		PIXEL(20, 20, 20, 20),
-		PIXEL(21, 20, 20, 20),
-		PIXEL(22, 20, 20, 20),
-		PIXEL(23, 20, 20, 20),
-		PIXEL(24, 20, 20, 20),
-		PIXEL(25, 20, 20, 20),
-		PIXEL(26, 20, 20, 20),
-		PIXEL(27, 20, 20, 20),
-		PIXEL(28, 20, 20, 20)
+		PIXEL(2, 213, 255, 255),
+		PIXEL(3, 0, 0, 50),
+		PIXEL(4, 0, 0, 50),
+		PIXEL(5, 0, 0, 50),
+		PIXEL(6, 0, 0, 50),
+		PIXEL(7, 0, 0, 50),
+		PIXEL(8, 0, 0, 50),
+		PIXEL(9, 0, 0, 50),
+		PIXEL(10, 0, 0, 50),
+		PIXEL(11, 0, 0, 50),
+		PIXEL(12, 0, 0, 50),
+		PIXEL(13, 0, 0, 50),
+		PIXEL(14, 0, 0, 50),
+		PIXEL(15, 0, 0, 50),
+		PIXEL(16, 0, 0, 50),
+		PIXEL(17, 0, 0, 50),
+		PIXEL(18, 0, 0, 50),
+		PIXEL(19, 0, 0, 50),
+		PIXEL(20, 0, 0, 50),
+		PIXEL(21, 0, 0, 50),
+		PIXEL(22, 0, 0, 50),
+		PIXEL(23, 0, 0, 50),
+		PIXEL(24, 0, 0, 50),
+		PIXEL(25, 0, 0, 50),
+		PIXEL(26, 0, 0, 50),
+		PIXEL(27, 0, 0, 50),
+		PIXEL(28, 213, 255, 255)
 	)
 };
 
@@ -414,19 +412,33 @@ volatile unsigned long currentTime;
 bool DRLAccesa = true;
 
 void setup() {
-	//Serial.begin(9600);
+	Serial.begin(9600);
 
   	pinMode(5, INPUT);
 
-	lxDRL.initialize();
+	lxDRL.initialize(&onAnimationEnd_LX);
 	//rxDRL.initialize();
 
-	lxDRL.setupAnimation(startup, STARTUP_FRAMES, 60);
-	lxDRL.startAnimation();
+	lxDRL.setupAnimation(16, startup, STARTUP_FRAMES, 60);
+	lxDRL.startAnimation(16);
 
-	//lxDRL.setupAnimation(blink, BLINK_FRAMES, 20);
-	//lxDRL.startAnimation();
-} 
+}
+
+void onAnimationEnd_LX(int animationId) {
+	Serial.print("LX Animation ");
+	Serial.print(animationId);
+	Serial.println(" ended");
+
+	switch(animationId) {
+		case 16: { //startup
+			lxDRL.setupAnimation(17, blink, BLINK_FRAMES, 30);
+			lxDRL.showDRL();
+			DRLAccesa = true;
+			//lxDRL.startAnimation(17);
+			break;
+		}
+	}
+}
 
 void loop() {
 	val = digitalRead(5); //1
@@ -440,22 +452,22 @@ void loop() {
 			DRLAccesa = false;
 			//Serial.println("ACCESO");
 			blinkTime = currentTime;
-			lxDRL.startAnimation();
+			lxDRL.startAnimation(17);
 		} else if(val == HIGH && blinkState == true) {
 			blinkState = false;
 			DRLAccesa = false;
 			//Serial.println("SPENTO");
-			blinkTime = currentTime;
+			//blinkTime = currentTime;
 			//lxDRL.stopAnimation();
 		}
   	}
 
-  	/*if(currentTime - blinkTime > 1000 && DRLAccesa == false) {
+  	if(currentTime - blinkTime > BLINK_FRAMES * 40 && DRLAccesa == false && val == HIGH) {
   		//Serial.println("FERMO DA TROPPO");
   		DRLAccesa = true;
   		lxDRL.reset();
   		lxDRL.showDRL();
-  	}*/
+  	}
 	
 
 	/* 
